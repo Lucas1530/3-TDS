2b280eb943f3de601fa1ef8e405535df9215520f
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-02-27 11:44:10 -0300

    Create funcao.php

---
+++ Linguagem Web II/Funcao/funcao.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+function subtrair(int $n1 , int $n2) : int {
+    $resultado = $n1 - $n2;
+    return $resultado;
+
+}
+
+//Programa principal
+ $resultado subtrair(8 , 3);
+echo $resultado . "<br>";
+echo subtrair(16 , 30);
+
+?>
+
