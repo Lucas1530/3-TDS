bf4f2bcfa0937c6f9f3cc8dbfbc5d443c8db8554
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-03-05 09:41:58 -0300

    Update circulo.php

--- Linguagem Web II/Aula3/circulo.php
+++ Linguagem Web II/Aula3/circulo.php
@@ -1,1 +1,31 @@
+<?php
 
+define("PI", 3.14);
+
+function area() {
+    $area = $raio * $raio * PI;
+    return $area;
+}
+
+function circunferencia($raio) {
+    return 2 * PI * $raio;
+}
+
+//Programa principal
+
+$r1 = 10;
+$area = area($r1);
+$cirfunf = circunferencia($r1);
+echo"Raio: " . $r1 . "cm Área: " . "cm Circunf:" . $cirfunf . "cm" . "<br>" . "<br>";
+
+$r1 = 20;
+$area = area($r1);
+$cirfunf = circunferencia($r1);
+echo"Raio: " . $r1 . "cm Área: " . "cm Circunf:" . $cirfunf . "cm" . "<br>" . "<br>";
+
+$r1 = 30;
+$area = area($r1);
+$cirfunf = circunferencia($r1);
+echo"Raio: " . $r1 . "cm Área: " . "cm Circunf:" . $cirfunf . "cm" . "<br>" ;
+
+?>
