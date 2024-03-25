af66092b44693a2c852e9f3bf59f17541a65aab8
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-03-25 11:02:00 -0300

    Update Lista2.php

--- Linguagem Web II/Lista2.php
+++ Linguagem Web II/Lista2.php
@@ -1,73 +1,41 @@
-<?php 
+<?php
 
-//Função
-function desenhar(array $linha) {
-
+//Função para desenhar uma linha da tabela
+function desenharLinha(array $linha) {
+  echo "<tr>";
+  foreach ($linha as $valor) {
+    echo "<td>$valor</td>";
+  }
+  echo "</tr>";
 }
 
+// A - Declarando os arrays associativos (um para cada linha)
+$linhas = array(
+  array("Ordem" => "1", "País" => "EUA", "ouro" => 46, "prata" => 37, "bronze" => 38, "total" => 121),
+  array("Ordem" => "2", "País" => "Grã-Bretanha", "ouro" => 27, "prata" => 23, "bronze" => 17, "total" => 67),
+  array("Ordem" => "3", "País" => "China", "ouro" => 26, "prata" => 18, "bronze" => 25, "total" => 70),
+  array("Ordem" => "4", "País" => "Rússia", "ouro" => 19, "prata" => 17, "bronze" => 20, "total" => 56),
+  array("Ordem" => "5", "País" => "Alemanha", "ouro" => 17, "prata" => 10, "bronze" => 15, "total" => 42),
+);
 
+// B - Criando a tabela
+echo "<table border='2'>";
 
-// A - Declarando os 5 arrays associativos (um para cada linha)
-$linha1 = array("Ordem" => "1",
-"Páis" => "EUA", "ouro" => 46,
-"prata" => 37, "bronze" => 38, "total" => 121,
-"bandeira" => "");
-
-$linha2 = array("Ordem" => "2",
-"Páis" => "Grã-Bretanha", "ouro" => 27,
-"prata" => 23, "bronze" => 17, "total" => 67,
-"bandeira" => "");
-
-
-
-$linha3 = array("Ordem" => "3",
-"Páis" => "China", "ouro" => 26,
-"prata" => 18, "bronze" => 25, "total" => 70,
-"bandeira" => "");
-
-$linha4 = array("Ordem" => "4",
-"Páis" => "Russia", "ouro" => 19,
-"prata" => 17, "bronze" => 20, "total" => 56,
-"bandeira" => "");
-
-$linha5 = array("Ordem" => "5",
-"Páis" => "Alemanha", "ouro" => 17,
-"prata" => 10, "bronze" => 15, "total" => 42,
-"bandeira" => "");
-
-$linha = array($linha1,$linha2,$linha3,$linha4,$linha5);
-
-echo " <table border = '2'>";
+// Cabeçalho da tabela
 echo "<tr>";
-echo "<td> Ordem </td>";
-echo "<td> Páis </td>";
-echo "<td> ouro </td>";
-echo "<td> prata </td>";
-echo "<td> bronze </td>";
-echo "<td> total </td>";
+echo "<th>Ordem</th>";
+echo "<th>País</th>";
+echo "<th>Ouro</th>";
+echo "<th>Prata</th>";
+echo "<th>Bronze</th>";
+echo "<th>Total</th>";
 echo "</tr>";
 
-
-for ($i= 0; $i < count($linha)  ; $i++) { 
-    echo"<tr>";
-    /*foreach ($linha[$i] as $p) {
-        echo "<td> $p </td>";
-    }*/
-    echo "<td>" . $linha[$i]["Ordem"] . "</td>";
-    echo "<td> <img src='link' />" . $linha[$i]["Páis"] . "</td>";
-
-    echo"</tr>";
+// C - Percorrer as linhas e gerar a tabela
+foreach ($linhas as $linha) {
+  desenharLinha($linha);
 }
 
-
-
-
-
-// B - Criar a matriz
-
-
-//C - Percorre a matriz para gerar as linhas da tabela (chamar função)
-
-
+echo "</table>";
 
 ?>
