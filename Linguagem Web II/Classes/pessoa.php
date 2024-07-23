cd7cfa26dc76a21ef430bbb1d8736cec3ae1b4b8
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-07-23 14:50:18 -0300

    Create pessoa.php

---
+++ Linguagem Web II/Classes/pessoa.php
@@ -0,0 +1,66 @@
+<?php
+// Declaração da Classe
+class Pessoa {
+
+    // Atributos
+    private $nome;
+    private $sobrenome;
+
+    // Construtor
+    public function __construct($nome="", $sobrenome=""){
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+    }
+
+    // Métodos
+    public function nomeCompleto() {
+        return $this->nome . ' ' . $this->sobrenome;
+    }
+
+    // GETs & SETs
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome() {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    /**
+     * Get the value of sobrenome
+     */
+    public function getSobrenome() {
+        return $this->sobrenome;
+    }
+
+    /**
+     * Set the value of sobrenome
+     */
+    public function setSobrenome($sobrenome): self {
+        $this->sobrenome = $sobrenome;
+        return $this;
+    }
+} // Fim da Classe Pessoa
+
+$pessoa1 = new Pessoa();
+$pessoa1->setNome("Lucas");
+$pessoa1->setSobrenome("Conradi");
+echo $pessoa1->nomeCompleto();
+echo '<br>';
+
+$pessoa2 = new Pessoa("Vinicius", "Da Paz");
+echo $pessoa2->nomeCompleto();
+echo '<br>';
+
+$pessoa3 = new Pessoa("Erick Dias", "Paulino");
+echo $pessoa3->nomeCompleto();
+
+?>
