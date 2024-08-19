53e81bc0eeab2c8a3c17cc05e65ccaecb93535f6
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-08-19 08:58:31 -0300

    Create form_exec.php

---
+++ Linguagem Web II/Aula7 HTML Formularios/pratica/form_exec.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Formulário submetido!</h1>";
+
+echo "<p>Nome escola:" . $_POST["nomeescola"] . "</p>";
+
+echo "<p>Nivel:" . $_POST["nivel"] . "</p>";
+
+echo "<a href='form.php'> voltar </a>";
+//GET MESMA COISA
