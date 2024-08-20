d75a2d3fac6622028ce663b07a98f1b47947d928
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-08-20 10:15:31 -0300

    Update veiculo.php

--- Linguagem Web II/Aula7 HTML Formularios/pratica/veiculo.php
+++ Linguagem Web II/Aula7 HTML Formularios/pratica/veiculo.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Veiculo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -16,7 +17,7 @@
         <input name="marca" placeholder="Informe a marca" />
         <br><br>
         <select name="combustivel">
-            <option value="">--Selecione o combustível---</option>
+            <option value="">Seleciona o combustível</option>
             <option value="A">Álcool</option>
             <option value="G">Gasolina</option>
             <option value="F">Flex</option>
