d7a8bbc251a3a364db2f5fa8306b158c516e6b7a
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-08-24 07:53:39 -0300

    Create login_exec.php

---
+++ Linguagem Web II/Aula7 HTML Formularios/Exercicios/Exerc2/login_exec.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IFPR</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+</body>
+</html>
+
+
+<?php
+class login{
+    private $user;
+    private $password;
+    
+    public function getUser()
+    {
+        return $this->user;
+    }
+    public function setUser($user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPassword($password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
+
+$user=$_POST["user"];
+$password=$_POST["password"];
+$lg = new login();
+$lg->setUser($user)->setPassword($password);
+
+
+if ($lg->getUser()=="IFPR"&&$lg->getPassword()=="TDS") {
+    echo"Login Sucesso <br>";
+    echo"Acesso liberado";
+}
+else {
+    echo"Login failed";
+} 
+echo'
+<br><a href="login_form.php">Voltar</a>';
