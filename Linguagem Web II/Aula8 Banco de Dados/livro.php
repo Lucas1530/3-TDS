c080b801e290bf442a5355c4255469d4a060df9a
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-08-27 12:01:57 -0300

    Create livro.php

---
+++ Linguagem Web II/Aula8 Banco de Dados/livro.php
@@ -0,0 +1,55 @@
+<?php
+//Configuração de ERRO 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Incluindo Caminho
+
+include_once("conexao.php");
+
+
+
+$conn = Conexao::getConexao();
+print_r($conn);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Cadastro de livros</title>
+</head>
+
+<body>
+    <h3>Formulário do livro</h3>
+    <form method="POST">
+
+        <input type="text" name="titulo" placeholder="Informe o título" />
+        <br><br>
+
+        <select name="genero">
+            <option value="">Seleciona Genero</option>
+            <option value="A">Aventura</option>
+            <option value="D">Drama</option>
+            <option value="F">Ficção</option>
+            <option value="R">Romance</option>
+            <option value="T">Terror</option>
+        </select>
+        <br><br>
+
+        <input type="number" name="qtdPag" placeholder="Informe a Quantidade de Páginas">
+        <br><br>
+
+        <input type="submit" class="btn btn-primary" value="Gravar" />
+        <input type="reset" class="btn btn-secondary" value="Limpar" />
+    </form>
+
+    <h3>Listagem de livros</h3>
+
+</body>
+
+</html>
