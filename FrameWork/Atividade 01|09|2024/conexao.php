215d26ee2e88a86a84a96584343b699631f5713b
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-09-16 11:42:05 -0300

    Update conexao.php

--- FrameWork/Atividade 01|09|2024/conexao.php
+++ FrameWork/Atividade 01|09|2024/conexao.php
@@ -1,17 +1,23 @@
 <?php
-include "criaClasses.php";
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-try{
-    $server=$_POST["servidor"];
-    $banco=$_POST["banco"];
-    $usuario=$_POST["usuario"];
-    $senha=$_POST["senha"];
-    $conn= new PDO("mysql:host=".$server.";dbname=$banco","$usuario","$senha");
-    criaClasses::criar($conn,$banco,$usuario,$senha);
-    header("Location:criarFormulario.php?bd=".$banco);
- }catch(PDOException $e){
-	echo "erro".$e->getMessage();
-    header("Location:frameworkMVC.php?msg=1");
+
+class Conexao {
+    public static function conectar() {
+        $host = 'localhost'; 
+        $dbname = 'framework'; 
+        $user = 'root'; 
+        $password = ''; 
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            exit; // Stop script execution on connection failure
+        }
+    }
 }
 ?>
