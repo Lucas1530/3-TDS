6421caf79d8228daea920fd241b48ba8f1a45e43
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-09-16 10:43:10 -0300

    Create conexao.php

---
+++ FrameWork/Trabalho DAO/conexao.php
@@ -0,0 +1,17 @@
+<?php
+class Conexao {
+    private static $con;
+
+    static function conectar() {
+        if (!self::$con) {
+            try {
+                self::$con = new PDO('mysql:host=localhost;dbname=loja', 'root', 'bancodedados');
+                self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo 'ERROR: ' . $e->getMessage();
+            }
+        }
+        return self::$con;
+    }
+}
+?>
