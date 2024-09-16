15cc1691ec49d47af0dee643e7a789a4eddee0bf
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-09-16 10:43:55 -0300

    Update conexao.php

--- FrameWork/Trabalho DAO/conexao.php
+++ FrameWork/Trabalho DAO/conexao.php
@@ -5,7 +5,7 @@ class Conexao {
     static function conectar() {
         if (!self::$con) {
             try {
-                self::$con = new PDO('mysql:host=localhost;dbname=loja', 'root', 'bancodedados');
+                self::$con = new PDO('mysql:host=localhost;dbname=loja', 'root', '');
                 self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 echo 'ERROR: ' . $e->getMessage();
