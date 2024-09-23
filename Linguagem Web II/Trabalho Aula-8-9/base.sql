d5eb13e362b641751cfc06bcd82648ff5f5a08fd
Lucas Conradi <116355015+Lucas1530@users.noreply.github.com>
2024-09-23 19:22:26 -0300

    Update base.sql

--- Linguagem Web II/Trabalho Aula-8-9/base.sql
+++ Linguagem Web II/Trabalho Aula-8-9/base.sql
@@ -1,62 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Tempo de geração: 17/09/2024 às 11:53
--- Versão do servidor: 8.0.36-0ubuntu0.22.04.1
--- Versão do PHP: 8.1.2-1ubuntu2.18
+CREATE DATABASE IF NOT EXISTS jogos_db;
+USE jogos_db;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `jogos_db`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `jogos`
---
-
-CREATE TABLE `jogos` (
-  `id` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `jogos` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `genero` enum('Ação','Aventura','Estratégia','RPG','Esporte','Simulação') NOT NULL,
   `plataforma` enum('PC','PlayStation','Xbox','Nintendo Switch') NOT NULL,
   `ano_lancamento` year NOT NULL,
-  `icone` varchar(255) DEFAULT NULL
+  `icone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `jogos`
---
-ALTER TABLE `jogos`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `jogos`
---
-ALTER TABLE `jogos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
